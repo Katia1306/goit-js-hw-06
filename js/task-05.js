1a7445f62838a32ef6ec326f1a105fc33a2efe70
Katia1306 <kateryna.zhylan@gmail.com>
2023-09-25 16:41:44 +0200

    update8

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,17 @@
 const output = document.querySelector("#name-output");
-const input = document.querySelector("name-input");
+const input = document.querySelector("#name-input");
 
-input.addEventListener("input", () => {
-
-
-  output.textContent = event.target.value||`Anonymous`;
+input.addEventListener("input", (event) => {
+   
+  output.textContent = event.target.value || `Anonymous`;
 
 });
 
-console.log(input);
+
+
+
+
+
+
+
+
---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+const inputValidator = document.querySelector("#validation-input");
+
+
+inputValidator.addEventListener('blur', () => {
+    const dataLength = Number(inputValidator.dataset.length);
+    const inputLength = inputValidator.value.length); }
+
+
+
+
+
+
+
+
+//validInput.addEventListener('blur', handlerGreeting);
+//function handlerGreeting(event) {
+  //validInput = event.currentTarget.value; }
