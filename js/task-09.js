c0f6278c028e7a29adf8403c9b2a78df39cfe418
Katia1306 <kateryna.zhylan@gmail.com>
2023-09-27 21:45:33 +0200

    update12

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const color = getRandomHexColor();
+
+btn.addEventListener('click',  () => {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = `${color}`
+});
+
+
